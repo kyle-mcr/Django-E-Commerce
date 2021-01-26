21fadfe0ada025f782ca713797b788519dce7cc8
Kyle McReynolds <kylemcreynolds92@gmail.com>
2021-01-26 01:54:28 +0000

    current ;ive site

--- perfectcushion/settings.py
+++ perfectcushion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.ultimatecards5.com', 'ultimatecards5.com', 'webapp-1020656.pythonanywhere.com']
 
